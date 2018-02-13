cef443f88a7d9d13dad5d672cc0bb08139d5c914
Jeremy McCoy <jmccoy@sovrn.com>
2018-02-13 12:18:03 -0700

    improved adherance to google style guide

--- advanced-looping-example.bash
+++ advanced-looping-example.bash
@@ -11,30 +11,19 @@ source extlib.bash
 trap - exit
 
 logline () {
-    # This function just prints lines passed to it, but could do anything.
-    local CURRENT_FUNC="logline"
-
-    #####
-    # Inputs
-    INPUTLINE="$*"
-    #####
-
-    log "DEBUG" "Input line: $INPUTLINE"
+  # This function just prints lines passed to it, but could do anything.
+  local CURRENT_FUNC="logline"
+  ##### v INPUTS v #####
+  INPUTLINE="$*"
+  ##### ^ INPUTS ^ #####
+  log "DEBUG" "Input line: $INPUTLINE"
 }
-
-# Export all functions and variables that need to be available to GNU Parallel and subshells
-export LOGFILE
-export SHELL=$(type -p bash) # So that Parallel knows to use BASH
-export -f pprint
-export -f log
-export -f logline
 
 # This is how you loop over all filenames in a directory in a linear fashion
 log "INFO" "Doing linear example"
-while read -r LINE
-do
-    # Log the line and strip any leading ./
-    logline "${LINE#./}"
+while read -r LINE ; do
+  # Log the line and strip any leading ./
+  logline "${LINE#./}"
 done <<< "$(find . -maxdepth 1 -type f)"
 # Notice how we are using reverse redirection of a subshell to feed the loop at the end.
 # We could do a forward pipe with `find | while read`, but
@@ -43,9 +32,15 @@ done <<< "$(find . -maxdepth 1 -type f)"
 # nor would failures/exit commands within properly kill the parent script.
 # Using reverse redirection from a limited subshell like this maintains variable scoping.
 
+# Export all functions and variables that need to be available to GNU Parallel and subshells
+export LOGFILE
+export SHELL=$(type -p bash) # So that Parallel knows to use BASH
+export -f pprint
+export -f log
+export -f logline
+
 # This is how you do the same thing, but instead feed the commands to GNU Parallel (if you have it installed)
-if which parallel > /dev/null
-then
+if which parallel > /dev/null ; then
     log "INFO" "Doing parallel example"
     find . -maxdepth 1 -type f | parallel logline
 else
--- extlib.bash
+++ extlib.bash
@@ -7,25 +7,27 @@ set -o pipefail
 # Set minimal config. You should overwrite this with better paths.
 PIDFILE="${PWD}/${0}.pid"
 LOGFILE="${PWD}/${0}.log"
+# The FINALCMDS array needs to be defined before setting up finally, but only if this is bash >=4.0
+[ ${BASH_VERSINFO[0]} -ge 4 ] && FINALCMDS=()
 
 pprint () {
-    # Function to properly wrap and print text
-    # Usage:
-    #   command | pprint
-    #       or
-    #   pprint <<< "text"
-    # Do not set CURRENT_FUNC here, as we want to inherit it
-    fold -sw "$(WIDTH="$(tput cols)" ; echo "${WIDTH:-80}")"
+  # Function to properly wrap and print text
+  # Usage:
+  #   command | pprint
+  #       or
+  #   pprint <<< "text"
+  # Do not set CURRENT_FUNC here, as we want to inherit it
+  fold -sw "$(WIDTH="$(tput cols)" ; echo "${WIDTH:-80}")"
 }
 
 log () {
-    # Function to send log output to stdout and file
-    # Usage:
-    #     command |& log $SEVERITY
-    #         or
-    #     log $SEVERITY $MESSAGE
-    # Do not set CURRENT_FUNC here, as we want to inherit it
-    date +"%x %X | ${0} [${CURRENT_FUNC:-SCRIPT_ROOT}] | ${1:-DEBUG} | ${2:-$(cat /dev/stdin)}" | tee -a "${LOGFILE}" | pprint >&2
+  # Function to send log output to stdout and file
+  # Usage:
+  #     command |& log $SEVERITY
+  #         or
+  #     log $SEVERITY $MESSAGE
+  # Do not set CURRENT_FUNC here, as we want to inherit it
+  date +"%x %X | ${0} [${CURRENT_FUNC:-SCRIPT_ROOT}] | ${1:-DEBUG} | ${2:-$(cat /dev/stdin)}" | tee -a "${LOGFILE}" | pprint >&2
 }
 
 # Shorthand log functions
@@ -35,81 +37,62 @@ log_warn () { log "WARN" "$*" ; }
 log_err () { log "ERROR" "$*" ; }
 
 quit () {
-    # Function to log a message and exit
-    # Usage:
-    #    quit $SEVERITY $MESSAGE $EXITCODE
-    local CURRENT_FUNC="quit"
-    log "${1:-WARN}" "${2:-Exiting without reason}"
-    exit "${3:-3}"
+  # Function to log a message and exit
+  # Usage:
+  #    quit $SEVERITY $MESSAGE $EXITCODE
+  local CURRENT_FUNC="quit"
+  log "${1:-WARN}" "${2:-Exiting without reason}"
+  exit "${3:-3}"
 }
-
-# Trap for killing runaway processes and exiting
-trap "quit 'UNKNOWN' 'Exiting on signal' '3'" SIGINT SIGTERM
 
 bash4check () {
-    # Call this function to enable features that depend on bash 4.0+.
-    # Do not set CURRENT_FUNC here, as we want to inherit it
-    if [ ${BASH_VERSINFO[0]} -lt 4 ]
-    then
-        echo "Sorry, you need at least bash version 4.0 to run this function: $CURRENT_FUNC" >&2
-        return 1
-    fi
+  # Call this function to enable features that depend on bash 4.0+.
+  # Do not set CURRENT_FUNC here, as we want to inherit it
+  if [ ${BASH_VERSINFO[0]} -lt 4 ] ; then
+    echo "Sorry, you need at least bash version 4.0 to run this function: $CURRENT_FUNC" >&2
+    return 1
+  fi
 }
-
-# The array needs to be defined before setting up finally, but only if this is bash >=4.0
-[ ${BASH_VERSINFO[0]} -ge 4 ] && FINALCMDS=()
 
 finally () {
-    # Function to perform final tasks before exit
-    local CURRENT_FUNC="finally"
-    bash4check
-    if [ "${#FINALCMDS[@]}" != 0 ]
-    then
-        for CMD in "${FINALCMDS[@]}"
-        do
-            eval "${CMD}"
-        done
-    else
-        log "WARN" "The array of final tasks was empty. Please ensure you set FINALCMDS or run `trap - exit` somewhere in your script."
-    fi
+  # Function to perform final tasks before exit
+  local CURRENT_FUNC="finally"
+  bash4check
+  if [ "${#FINALCMDS[@]}" != 0 ] ; then
+    for CMD in "${FINALCMDS[@]}" ; do
+      eval "${CMD}"
+    done
+  else
+    log "WARN" "The array of final tasks was empty. Please ensure you set FINALCMDS or run `trap - exit` somewhere in your script."
+  fi
 }
 
-# Trap to do final tasks before exit
-[ ${BASH_VERSINFO[0]} -ge 4 ] && trap finally EXIT
-
 checkpid () {
-    # Check for and maintain pidfile
-    local CURRENT_FUNC="checkpid"
-    bash4check
-    if [ -f "${PIDFILE}" ] && [ -d "/proc/$(cat "${PIDFILE}")" ]
-    then
-        quit "WARN" "This script is already running, exiting"
-    else
-        echo $$ > "${PIDFILE}"
-        FINALCMDS+=("rm -v '${PIDFILE}'")
-    fi
+  # Check for and maintain pidfile
+  local CURRENT_FUNC="checkpid"
+  bash4check
+  if [ -f "${PIDFILE}" ] && [ -d "/proc/$(cat "${PIDFILE}")" ] ; then
+    quit "WARN" "This script is already running, exiting"
+  else
+    echo $$ > "${PIDFILE}"
+    FINALCMDS+=("rm -v '${PIDFILE}'")
+  fi
 }
 
 requireuser () {
-    # Checks to see if current user matches $REQUIREUSER and exits if not.
-    # REQUIREUSER can be set as a variable or passed in as an argument.
-    local CURRENT_FUNC="requireuser"
-
-    #####
-    # Inputs
-    if [ -n $* ]
-    then
-        local REQUIREUSER="$*"
-    fi
-    #####
-
-    if [ -z $REQUIREUSER ]
-    then
-        quit "ERROR" "REQUIREUSER is not set"
-    elif [ "$REQUIREUSER" != "$USER" ]
-    then
-        quit "ERROR" "Only $REQUIREUSER is allowed to run this script"
-    fi
+  # Checks to see if current user matches $REQUIREUSER and exits if not.
+  # REQUIREUSER can be set as a variable or passed in as an argument.
+  local CURRENT_FUNC="requireuser"
+  ##### v INPUTS v #####
+  if [ -n $* ] ; then
+    local REQUIREUSER="$*"
+  fi
+  ##### ^ INPUTS ^ #####
+  if [ -z $REQUIREUSER ] ; then
+    quit "ERROR" "REQUIREUSER is not set"
+  elif [ "$REQUIREUSER" != "$USER" ] ; then
+    quit "ERROR" "Only $REQUIREUSER is allowed to run this script"
+  fi
 }
 
 usage () {
@@ -127,23 +110,27 @@ HERE
 }
 
 argparser () {
-    # Accept command-line arguments
-    # You must pass "$@" as the argument to this function for it to work
-    # More info here: http://wiki.bash-hackers.org/howto/getopts_tutorial
-    local CURRENT_FUNC="argparser"
-    while getopts ":shvx" OPT
-    do
-        case ${OPT} in
-            h) usage ;;
-            s) source "${OPTARG}" ;;
-            v) set -x ;;
-            *) quit "ERROR" "Invalid option: '-${OPTARG}'. For usage, try '${0} -h'." ;;
-        esac
-    done
+  # Accept command-line arguments
+  # You must pass "$@" as the argument to this function for it to work
+  # More info here: http://wiki.bash-hackers.org/howto/getopts_tutorial
+  local CURRENT_FUNC="argparser"
+  while getopts ":shvx" OPT ; do
+    case ${OPT} in
+      h) usage ;;
+      s) source "${OPTARG}" ;;
+      v) set -x ;;
+      *) quit "ERROR" "Invalid option: '-${OPTARG}'. For usage, try '${0} -h'." ;;
+    esac
+  done
 }
 
+# Trap for killing runaway processes and exiting
+trap "quit 'UNKNOWN' 'Exiting on signal' '3'" SIGINT SIGTERM
+
+# Trap to do final tasks before exit
+[ ${BASH_VERSINFO[0]} -ge 4 ] && trap finally EXIT
+
 # If a .conf file exists for this script, source it
-if [ -f "${0}.conf" ]
-then
-    source "${0}.conf"
+if [ -f "${0}.conf" ] ; then
+  source "${0}.conf"
 fi
