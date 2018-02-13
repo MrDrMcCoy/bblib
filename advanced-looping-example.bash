a013e6b071ba1e1308689e9ef818a9a4d124812c
Jeremy McCoy <jmccoy@sovrn.com>
2018-02-12 17:42:58 -0700

    fixed n00b mistake in advanced example

--- advanced-looping-example.bash
+++ advanced-looping-example.bash
@@ -32,18 +32,25 @@ export -f logline
 
 # This is how you loop over all filenames in a directory in a linear fashion
 log "INFO" "Doing linear example"
-while read PATH
+while read -r LINE
 do
     # Log the line and strip any leading ./
-    logline "${PATH#./}"
+    logline "${LINE#./}"
 done <<< "$(find . -maxdepth 1 -type f)"
 # Notice how we are using reverse redirection of a subshell to feed the loop at the end.
-# We could do a forward pipe with `find | while read`,
-# but this would result in the whole while loop living in a subshell.
-# Howeer, that subshell wouldn't be able to pass variables back up to the main script,
-# nor would failures/exit commands properly kill the parent script.
+# We could do a forward pipe with `find | while read`, but
+# this would result in the whole while loop living in a subshell.
+# Because of this, that subshell wouldn't be able to pass variables back up to the main script,
+# nor would failures/exit commands within properly kill the parent script.
 # Using reverse redirection from a limited subshell like this maintains variable scoping.
 
 # This is how you do the same thing, but instead feed the commands to GNU Parallel (if you have it installed)
-log "INFO" "Doing parallel example"
-parallel logline <<< "$(find . -maxdepth 1 -type f)"
+if which parallel > /dev/null
+then
+    log "INFO" "Doing parallel example"
+    find . -maxdepth 1 -type f | parallel logline
+else
+    log "WARN" "GNU Parallel is not installed"
+fi
+# Notice how we are doing it the "normal" way here. Since we will be in a subshell anyway, the pipe method is more readable.
+# Just remember that you have to be extra careful about variable scoping this way.
