442fa043d56701557c2907c5dd5b2f9f43a1ec75
Jeremy McCoy <jmccoy@sovrn.com>
2018-03-12 13:18:19 -0600

    expanded example

--- example.bash
+++ example.bash
@@ -16,6 +16,18 @@ requireuser
 # Add task to run on exit
 FINALCMDS+=("example_finally")
 
+# Add jobs for parallel execution
+JOBQUEUE+=(
+  'echo "Job ran in parallel with PID $BASHPID"'
+  'echo "Job ran in parallel with PID $BASHPID"'
+  'echo "Job ran in parallel with PID $BASHPID"'
+  'echo "Job ran in parallel with PID $BASHPID"'
+  'echo "Job ran in parallel with PID $BASHPID"'
+  'echo "Job ran in parallel with PID $BASHPID"'
+  'echo "Job ran in parallel with PID $BASHPID"'
+  'echo "Job ran in parallel with PID $BASHPID"'
+)
+
 #####
 # If you wish to propagate variables and functions to subshells or GNU Parallel, you will need to export them like so:
 #     export VARIABLE_NAME
@@ -26,14 +38,19 @@ main () {
   #####
   # Put your actions here! It is good practice to keep all logic in functions where possible
   #####
+  # Set the name of this function for the logger
   local CURRENT_FUNC="main"
+  # Log a test message
   log_info "Starting tasks"
+  # Run the example function
   example_function stuff things
+  # Log a message that shows a variable
   log_debug "EXAMPLEVAR = ${EXAMPLEVAR}"
+  # Run the queued jobs from JOBQUEUE in parallel
+  prunner
   # ...
 }
 
 # Run main
-log 'INFO' 'Starting main...'
 main
 quit 'INFO' 'All tasks completed successfully'
