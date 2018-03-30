3a0891f98994e739eaefa50bb8cb7bea000cfcec
Jeremy McCoy <jeremy@awesomegeek.com>
2018-03-30 11:31:51 -0600

    1.1.3 (#12)
    * reduced overly-verbose logging
    
    * updated readme
    
    * set null default for variable to avoid error
    
    * set null default for variable to avoid error
    
    * changed requireuser to use EUID

--- bblib.bash
+++ bblib.bash
@@ -119,7 +119,7 @@ log () {
   # Send message to logger
   if [ "${NUMERIC_SEVERITY}" -le "${NUMERIC_LOGLEVEL}" ] ; then
     tr '\n' ' ' <<< "${LOGMSG}" | logger -s -p "user.${NUMERIC_SEVERITY}" -t "${LOGTAG} " |& \
-      if [ -n "${LOGFILE}" ] ; then
+      if [ -n "${LOGFILE:-}" ] ; then
         tee -a "${LOGFILE}" | pprint ${LOGCOLORS[$NUMERIC_SEVERITY]}
       elif [ ! -t 0 ]; then
         pprint ${LOGCOLORS[$NUMERIC_SEVERITY]} < /dev/stdin
@@ -182,13 +182,13 @@ requireuser () {
   # Checks to see if current user matches $REQUIREUSER and exits if not.
   # REQUIREUSER can be set as a variable or passed in as an argument.
   # Usage: requireuser [user]
-  local REQUIREUSER="${1:-$REQUIREUSER}"
-  if [ -z "$REQUIREUSER" ] ; then
+  local REQUIREUSER="${1:-${REQUIREUSER:-}}"
+  if [ -z "${REQUIREUSER:-}" ] ; then
     quit "ERROR" "requireuser was called, but \$REQUIREUSER is not set"
-  elif [ "$REQUIREUSER" != "$USER" ] ; then
+  elif [ "$REQUIREUSER" != "$EUID" ] ; then
     quit "ERROR" "Only $REQUIREUSER is allowed to run this script"
   else
-    log "DEBUG" "User '$USER' matches '$REQUIREUSER' and is allowed to run this script"
+    log "DEBUG" "User '$EUID' matches '$REQUIREUSER' and is allowed to run this script"
   fi
 }
 
